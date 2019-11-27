caf366416f0f0425cc255ba7389a5e1dbd51280f
imicaustria <ic.austria@ymail.com>
2019-11-27 12:29:31 -0500

    fourth

--- js/index.js
+++ js/index.js
@@ -6,17 +6,17 @@ const product1 = {
     stock: `1 left!`
 };
 
-function getContainersAsHtmlString(containers) {
+function getContainersAsHtmlString(container) {
     return `<article class="container">
-                <a href="product.html"> <img src="${containers.img}" alt="Product Image">  </a>
+                <a href="product.html"> <img src="${container.img}" alt="Product Image">  </a>
                 <div class="intro">
-                    <h3>${containers.name}</h3>
-                    <span><del>$50.00</del>       <ins>${containers.price}</ins></span>
+                    <h3>${container.name}</h3>
+                    <span><del>$50.00</del>       <ins>${container.price}</ins></span>
                 </div>
         
                 <section class="deetspro1">
                     <ul>
-                        <li><b>${containers.descrp}</b> </li>
+                        <li><b>${container.descrp}</b> </li>
                         <li><input type="radio"> <span>Light Brown</span> <input type="radio">  <span> Indigo</span><input type="radio">  <span>White</span></li>
                         <li><input type="radio"> <span>M</span> <input type="radio">  <span>L</span> <input type="radio">  <span>XL</span></li>
                         <li><span><i class="material-icons">star</i><i class="material-icons">star</i><i class="material-icons">star</i><i class="material-icons">star</i><i class="material-icons">star_half</i></span></li>
@@ -26,12 +26,26 @@ function getContainersAsHtmlString(containers) {
                 <section class="final">
                     <button class="check"> Add to Bag </button>
                     <button class="fave" type="button"><i class="material-icons">favorite_border</i></button>
-                    <p>${containers.img}</p>
+                    <p>${container.img}</p>
                 </section>
             </article>`;
 }
 
-document.getElementById('container').innerHTML = getContainersAsHtmlString (product1)
+document.getElementById('container').innerHTML = getContainerAsHtmlString (product1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
