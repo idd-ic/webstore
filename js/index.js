f8feb55079102936fe3cd23a496c22350792774d
imicaustria <ic.austria@ymail.com>
2019-12-10 10:38:28 -0500

    seventh

--- js/index.js
+++ js/index.js
@@ -4,145 +4,167 @@ const container = [
         name: `That Dress Tho`,
         regprice: `59.00`,
         price: `39.00`,
-        descrp: `Very comfy sleeveless corduroy dress`,
-        stock: `Snag the last one!`
+        descrp: `Very comfy sleeveless corduroy dress.`,
+        stock: `Snag the last one!`,
+        category: `dress`
     },{
         img: `img/o.brown.jpg`,
         name: `Modern Jacket`,
         regprice: `89.00`,
         price: `69.00`,
-        descrp: `Perfect for spring and layering during winter`,
-        stock: `Hurry! 10 stocks left!`
+        descrp: `Perfect for spring and layering!`,
+        stock: `Hurry! 10 stocks left!`,
+        category: `outer`
     },{
         img: `img/p.pink.jpg`,
         name: `Mean Girl Pants`,
         regprice: `159.00`,
         price: `129.00`,
         descrp: `A very beautiful flare corduroy pants.`,
-        stock: `Hurry! 15 stocks left!`
+        stock: `Hurry! 15 stocks left!`,
+        category: `pants`
     },{
         img: `img/s.grey.jpg`,
         name: `Where'd You Get That Skirt`,
         regprice: `59.00`,
         price: `49.00`,
         descrp: `A chic vintage-inspired skirt`,
-        stock: `LAST CHANCE!`
+        stock: `LAST CHANCE!`,
+        category: `skirt`
     },{
         img: `img/sw.turquoise.jpg`,
         name: `Spring Sweater`,
         regprice: `49.00`,
         price: `39.00`,
         descrp: `A mockneck corduroy sweater.`,
-        stock: `Snag the last one!`
+        stock: `Snag the last one!`,
+        category: `sweaters`
     },{
         img: `img/t.yellow.jpg`,
         name: `Spaghetti Top`,
         regprice: `49.00`,
         price: `19.00`,
         descrp: `A very flattering spaghetti strap top.`,
-        stock: `Hurry! Low on stocks!`
+        stock: `Hurry! Low on stocks!`,
+        category: `tops`
     },{
         img: `img/d.cblock.jpg`,
         name: `Color block dress`,
         regprice: `99.00`,
         price: `79.00`,
         descrp: `An interestingly designed dress.`, 
-        stock: `Hurry! 10 stocks left!`
+        stock: `Hurry! 10 stocks left!`,
+        category: `dress`
     },{
         img: `img/p.cigarette.jpg`,
         name: `Cigarette Cropped Pants`,
         regprice: `89.00`,
         price: `59.00`,
         descrp: `This'll fit well if you're over 5ft tall.`,
-        stock: `LAST CHANCE!`
+        stock: `LAST CHANCE!`,
+        category: `pants`
     },{
         img: `img/p.cropped.jpg`,
         name: `Cropped Skinny Pants`,
         regprice: `79.00`,
         price: `49.00`,
         descrp: `Perfect for those ankles!`,
-        stock: `LAST CHANCE!`
+        stock: `LAST CHANCE!`,
+        category: `pants`
     },{
         img: `img/d.peplum.jpg`,
         name: `Fuchsia Peplum Dress`,
         regprice: `199.00`,
         price: `159.00`,
         descrp: `To when she's feeling a bit conservative.`,
-        stock: `LAST CHANCE!`
+        stock: `LAST CHANCE!`,
+        category: `peplum`
     },{
         img: `img/d.aline.jpg`,
         name: `Very cute a-line buttoned dress.`,
         regprice: `129.00`,
         price: `99.00`,
         descrp: `Not too short but critical when it winds.`,
-        stock: `LAST CHANCE!`
+        stock: `LAST CHANCE!`,
+        category: `dress`
     },{
         img: `img/o.db.jpg`,
         name: `A dark brown modern jacket.`,
         regprice: `89.00`,
         price: `69.00`,
         descrp: `Perfect for spring and layering during winter.`,
-        stock: `LAST CHANCE!`
+        stock: `LAST CHANCE!`,
+        category: `outer`
     },{
         img: `img/d.orange.jpg`,
         name: `That Dress Tho`,
         regprice: `59.00`,
         price: `39.00`,
         descrp: `Very comfy sleeveless corduroy dress.`,
-        stock: `LAST CHANCE!` 
+        stock: `LAST CHANCE!`,
+        category: `dress` 
     },{
         img: `img/d.shaper.jpg`,
         name: `That Dress Tho`,
         regprice: `159.00`,
         price: `139.00`,
         descrp: `To when she's feeling slightly conservative.`,
-        stock: `LAST CHANCE!`
+        stock: `LAST CHANCE!`,
+        category: `dress`
     },{
         img: `img/p.taupe.jpg`,
         name: `Mean Girl Pants`,
         regprice: `159.00`,
         price: `129.00`,
         descrp: `A very beautiful flare corduroy pants.`,
-        stock: `LAST CHANCE!`
+        stock: `LAST CHANCE!`,
+        category: `pants`     
     },{
         img: `img/t.black.jpg`,
         name: `Spaghetti Top`,
         regprice: `49.00`,
         price: `19.00`,
         descrp: `A very flattering spaghetti strap top.`,
-        stock: `Hurry! Low on stocks!`
+        stock: `Hurry! Low on stocks!`,
+        category: `tops`
     },{
         img: `img/t.buckle.jpg`,
         name: `Moss green buckle strap top!`,
         regprice: `59.00`,
         price: `29.00`,
         descrp: `Perfect when you're trying to be cool like Lara Croft. `,
-        stock: `Hurry! Low on stocks!`
+        stock: `Hurry! Low on stocks!`,
+        category: `tops`
     },{
         img: `img/p.green.jpg`,
         name: `Mean Girl Pants`,
         regprice: `159.00`,
         price: `129.00`,
         descrp: `A very beautiful flare corduroy pants.`,
-        stock: `Hurry! Low on stocks!`
+        stock: `Hurry! Low on stocks!`,
+        category: `pants`
     },{
         img: `img/p.straight.jpg`,
         name: `The corporate straight-cut corduroy pants.`,
         regprice: `229.00`,
         price: `189.00`,
         descrp: `Fool people and look professional with this pants!`,
-        stock: `Hurry! Low on stocks!`
+        stock: `Hurry! Low on stocks!`,
+        category: `pants`
     },{
         // img: `img/s.redb.jpg`,
-        //     name: `Where'd You Get That Skirt with Buckle Edition`,
-        //     regprice: `89.00`,
-        //     price: `69.00`,
-        //     descrp: `This could have been a partner of the buckle top if this wasn't red.`,
-        //     stock: `LAST CHANCE!
+        // name: `Where'd You Get That Skirt with Buckle Edition`,
+        // regprice: `89.00`,
+        // price: `69.00`,
+        // descrp: `This could have been a partner of the buckle top if this wasn't red.`,
+        // stock: `LAST CHANCE!,
+        // category: `pants`
     }
 ]
 
-/// If the cost is less than $50, tell the user this is a LAST CHANCE
+
+
+/************* FUNCTIONS *************/
 
 function getContainerAsHtmlString(container) {
     return `<article class="box one">
@@ -165,6 +187,7 @@ function getContainerAsHtmlString(container) {
                 <section class="final">
                     <button class="check"> Add to Bag </button>
                     <a href="#"><button><i class="material-icons">favorite_border</i></button></a>
+                </section>
             </article>`;
 };
 
@@ -178,16 +201,27 @@ renderContainer (container);
 
 
 
-// document.getElementById('container').innerHTML = getContainerAsHtmlString (product1);
-// document.getElementById('container').innerHTML += getContainerAsHtmlString (product2);
-// document.getElementById('container').innerHTML += getContainerAsHtmlString (product3);
-// document.getElementById('container').innerHTML += getContainerAsHtmlString (product4);
-// document.getElementById('container').innerHTML += getContainerAsHtmlString (product5);
-// document.getElementById('container').innerHTML += getContainerAsHtmlString (product6);
 
 
 
 
+
+//toggle view from list to grid and vice-versa
+
+function toggleProducts(event) {
+    document.getElementById('container').classList.toggle('grid-view');
+}
+
+//sort product by price
+
+
+//search product by name
+
+
+
+/************* EXECUTABLE *************/
+
+document.getElementById(`toggle`).addEventListener(`click`, toggleProducts);
 
 
 
